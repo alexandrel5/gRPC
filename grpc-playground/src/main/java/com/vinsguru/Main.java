539e691bcab635a14e514c86eab9369554f6735e
“Alexandre <alexandrecandido@visual.com.br>
2025-11-29 17:13:52 -0300

    Removing unused Main

--- grpc-playground/src/main/java/com/vinsguru/Main.java
+++
@@ -1,9 +0,0 @@
-package com.vinsguru;
-
-import com.vinsguru.models.PersonOuterClass;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-}
