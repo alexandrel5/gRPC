55de3c0f34ba76caaf32594e2af663db49c68968
Alexandre Candido <alexandrecandido@visual.com.br>
2025-12-02 19:27:23 -0300

    gRPC Server Refactor

--- grpc-playground/src/main/java/com/vinsguru/sec06/BankService.java
+++ grpc-playground/src/main/java/com/vinsguru/sec06/BankService.java
@@ -3,6 +3,7 @@ package com.vinsguru.sec06;
 import com.vinsguru.models.sec06.AccountBalance;
 import com.vinsguru.models.sec06.BalanceCheckRequest;
 import com.vinsguru.models.sec06.BankServiceGrpc;
+import com.vinsguru.sec06.repository.AccountRepository;
 import io.grpc.stub.StreamObserver;
 
 public class BankService extends BankServiceGrpc.BankServiceImplBase{
@@ -10,9 +11,12 @@ public class BankService extends BankServiceGrpc.BankServiceImplBase{
     @Override
     public void getAccountBalance(BalanceCheckRequest request, StreamObserver<AccountBalance> responseObserver) {
         var accountsNumber = request.getAccountNumber();
+
+        var balance = AccountRepository.getBalance(accountsNumber);
+
         var accontsBalance = AccountBalance.newBuilder()
                 .setAccountNumber(accountsNumber)
-                .setBalance(accountsNumber * 10)
+                .setBalance(balance)
                 .build();
         responseObserver.onNext(accontsBalance);
         responseObserver.onCompleted();
