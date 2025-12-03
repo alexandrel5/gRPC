7d367b6005c87bf096b74e6b906225b2f94c5a51
Alexandre Candido <alexandrecandido@visual.com.br>
2025-12-03 08:58:42 -0300

    gRPC Client Demo

---
+++ grpc-playground/src/main/java/com/vinsguru/sec06/GrpcClient.java
@@ -0,0 +1,22 @@
+package com.vinsguru.sec06;
+
+import com.vinsguru.models.sec06.BalanceCheckRequest;
+import com.vinsguru.models.sec06.BankServiceGrpc;
+import io.grpc.ManagedChannelBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class GrpcClient {
+    private static final Logger log = LoggerFactory.getLogger(GrpcClient.class);
+
+    public static void main(String[] args) {
+        var channel  = ManagedChannelBuilder.forAddress("localhost", 6565)
+                .usePlaintext()
+                .build();
+
+        var stub = BankServiceGrpc.newBlockingStub(channel);
+
+        var balance = stub.getAccountBalance(BalanceCheckRequest.newBuilder().setAccountNumber(2).build());
+        log.info("Balance is {}", balance);
+    }
+}
