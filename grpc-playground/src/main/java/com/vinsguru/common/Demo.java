a0ec51655862d57c16b7e894cc248442f7ac6897
Alexandre Candido <alexandrecandido@visual.com.br>
2025-12-08 13:27:23 -0300

    Implementing BIDirectional Streaming Service

--- grpc-playground/src/main/java/com/vinsguru/common/Demo.java
+++ grpc-playground/src/main/java/com/vinsguru/common/Demo.java
@@ -1,11 +1,12 @@
 package com.vinsguru.common;
 
 import com.vinsguru.sec06.BankService;
+import com.vinsguru.sec06.TransferService;
 
 public class Demo {
 
     public static void main(String[] args) {
-        GrpcServer.create(new BankService())
+        GrpcServer.create(new BankService(), new TransferService())
                 .start()
                 .await();
     }
